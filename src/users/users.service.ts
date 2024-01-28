990c24c8baa58da2d42a23e0357752289df445d4
cfcife <aboludepeter@gmail.com>
2024-01-28 01:25:00 +0100

    Add DTOs and update methods in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -41,42 +43,31 @@ export class UsersService {
     return user;
   }
 
-  create(user: {
-    name: string;
-    email: string;
-    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
-  }) {
+  create(createUserDto: CreateUserDto) {
     const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
     const newUser = {
       id: usersByHighestId[0].id + 1,
-      ...user,
+      ...createUserDto,
     };
     this.users.push(newUser);
     return newUser;
   }
 
-  update(
-    id: number,
-    updatedUser: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
-  ) {
+  update(id: number, updatedUserDTO: UpdateUserDto) {
     this.users = this.users.map((user) => {
       if (user.id === id) {
-        return { ...user, ...updatedUser };
+        return { ...user, ...updatedUserDTO };
       }
-      return user
-    })
+      return user;
+    });
 
-    return this.findOne(id)
+    return this.findOne(id);
   }
 
-  delete(id: number){
-    const removedUser = this.findOne(id)
+  delete(id: number) {
+    const removedUser = this.findOne(id);
 
-    this.users = this.users.filter(user => user.id !== id)
+    this.users = this.users.filter((user) => user.id !== id);
 
     return removedUser;
   }
