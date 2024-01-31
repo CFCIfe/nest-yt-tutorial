3cc63c033d613e2a231bd6f04dbb8244ba826214
Peter Abolude <aboludepeter@gmail.com>
2024-01-31 12:44:18 +0000

    Fix: Undefined in log

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,24 +1,14 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-  Ip,
-} from '@nestjs/common';
-import { EmployeesService } from './employees.service';
-import { Prisma } from '@prisma/client';
-import { Throttle, SkipThrottle } from '@nestjs/throttler';
-import { MyLoggerService } from 'src/my-logger/my-logger.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Ip } from "@nestjs/common";
+import { EmployeesService } from "./employees.service";
+import { Prisma } from "@prisma/client";
+import { Throttle, SkipThrottle } from "@nestjs/throttler";
+import { MyLoggerService } from "src/my-logger/my-logger.service";
 
 @SkipThrottle()
-@Controller('employees')
+@Controller("employees")
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
-  private readonly logger = new MyLoggerService(EmployeesService.name)
+  private readonly logger = new MyLoggerService(EmployeesService.name);
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
@@ -26,27 +16,24 @@ export class EmployeesController {
 
   @SkipThrottle({ default: false })
   @Get()
-  findAll(@Ip() ip: string, @Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    this.logger.log(`Request for ALL employees\t ${ip}`)
+  findAll(@Ip() ip: string, @Query("role") role?: "INTERN" | "ENGINEER" | "ADMIN") {
+    this.logger.log(`Request for ALL employees\t ${ip}`, EmployeesController.name);
     return this.employeesService.findAll(role);
   }
 
   @Throttle({ short: { ttl: 1000, limit: 2 } })
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.employeesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput,
-  ) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput) {
     return this.employeesService.update(+id, updateEmployeeDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.employeesService.remove(+id);
   }
 }
